2e594027a94a06c40cd7fc8eb0af689eb65e613f
HuBeyoung <274416303@qq.com>
2020-05-10 21:06:32 +0800

    Add files via upload

--- example/1_HelloEigen/HelloEigen.cpp
+++ example/1_HelloEigen/HelloEigen.cpp
@@ -8,7 +8,16 @@
 using namespace Eigen;
 using namespace std;
 
+// template <typename T>
+// void FunctionEight(const T* const k) {
+//    //    Map<Matrix<T, 3, 1>> kk(k);
+//    cout << *k << endl;
+//}
+
 int main(int argc, char* argv[]) {
+    /**
+      * 1. basic definition
+      * */
     //    MatrixXd m(2, 2);
     //    m(0, 0) = 3;
     //    m(1, 0) = 2.5;
@@ -16,6 +25,9 @@ int main(int argc, char* argv[]) {
     //    m(1, 1) = m(0, 1) + m(1, 0);
     //    cout << m << endl;
 
+    /**
+      * 1. MatrixXd and Random
+      * */
     //    MatrixXd pos = MatrixXd::Random(4, 2);
     //    pos << 1, 2, 3, 4, 5, 6, 7, 8;
     //    cout << pos.topRows(1) << endl;
@@ -35,6 +47,10 @@ int main(int argc, char* argv[]) {
     //            cout << posErr(i, j) << endl;
     //        }
     //    }
+
+    /**
+      * 3. Vector, dot, topRows, bottomRows, norm, acos
+      * */
     //    Vector2d osmVec(posErr(0, 0), posErr(0, 1));
     //    Vector2d optVec(posErr(1, 0), posErr(1, 1));
     //    double osmOptDot = osmVec.dot(optVec);
@@ -46,6 +62,9 @@ int main(int argc, char* argv[]) {
 
     //    cout << acos(0) << endl;
 
+    /**
+      * 4. VectorXd, RowMajor
+      * */
     //    double a = 0.39;
     //    double b = 0.522;
     //    double c = 5.84;
@@ -64,7 +83,9 @@ int main(int argc, char* argv[]) {
     //        }
     //    }
 
-    //    // Map
+    /**
+      * 5. Map
+      * */
     //    int mapVec[8];
     //    for (int i = 0; i < 8; ++i) {
     //        mapVec[i] = i;
@@ -102,14 +123,42 @@ int main(int argc, char* argv[]) {
     //    MatrixX2d p = MatrixX2d::Random(3, 2);
     //    p(0, 0) = 0.0;
 
-    MatrixXd k(3, 3);
-    k.setZero();
-    cout << k << endl;
+    /**
+      * 6. block
+      * */
+    //    MatrixXd k(3, 3);
+    //    k.setZero();
+    //    cout << k << endl;
 
-    Matrix<double, 3, 3> kk = Matrix<double, 3, 3>::Zero();
-    cout << "kk = " << endl << kk << endl;
+    //    Matrix<double, 3, 3> kk = Matrix<double, 3, 3>::Zero();
+    //    cout << "kk = " << endl << kk << endl;
 
-    Matrix3d kkk = Matrix3d::Zero();
-    cout << "kkk = " << endl << kkk << endl;
+    //    Matrix3d kkk = Matrix3d::Zero();
+    //    cout << "kkk = " << endl << kkk << endl;
+
+    //    k.block<2, 1>(0, 0) = Matrix<double, 2, 1>::Ones();
+    //    cout << k << endl;
+
+    /**
+      * 7. vecotr<Eigen::Matrix>
+      * */
+    //    std::vector<Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic>> R;
+    //    std::vector<double> r(20);
+    //    cout << "R.size = " << R.size() << endl;
+    //    R.resize(r.size());
+    //    cout << "R.size = " << R.size() << endl;
+
+    /**
+      * 8. function parameters
+      * */
+    //    FunctionEight(Matrix<double, 3, 1>(1234.5, 5234.3, 2334.4).data());
+
+    /**
+      * 9. can it work: (Eigen::Vector3d a - Eigen::Vector3d b).norm()
+      * */
+    Vector3d a = {1, 2, 3};
+    Vector3d b = {2, 3, 4};
+    cout << "(a - b).norm() = " << (a - b).norm() << endl;
+
     return 0;
 }
